a92eb96321458b52dbced2f599238b5342207095
Deasoso <932152571@qq.com>
2019-04-01 17:18:12 +0800

    basic.

--- src/router.js
+++ src/router.js
@@ -13,60 +13,13 @@ export default new Router({
       name: 'home',
       component: Home,
     },
-    {
-      path: '/about',
-      name: 'About',
-      // 路由级 code-splitting
-      // 这会给当前的路由页生成单独的块文件 (webpackChunkName 是 about 则得到 about.[版本哈希].js)
-      // 只有使用该 route 的场合才会下载这个页面的代码 (惰性加载).
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
-    {
-      path: '/article/:hash',
-      name: 'Article',
-      props: true,
-      component: () => import(/* webpackChunkName: "article" */ './views/Article.vue'),
-    },
-    {
-      path: '/article/:hash/comments',
-      name: 'Comments',
-      props: true,
-      component: () => import(/* webpackChunkName: "article-comments" */ './views/CommentsList.vue'),
-    },
     // {
-    //   path: '/login',
-    //   name: 'Login',
-    //   component: () => import(/* webpackChunkName: "login" */ './views/Login.vue'),
+    //   path: '/about',
+    //   name: 'About',
+    //   // 路由级 code-splitting
+    //   // 这会给当前的路由页生成单独的块文件 (webpackChunkName 是 about 则得到 about.[版本哈希].js)
+    //   // 只有使用该 route 的场合才会下载这个页面的代码 (惰性加载).
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     // },
-    {
-      path: '/user/:username',
-      name: 'User',
-      props: true,
-      component: () => import(/* webpackChunkName: "user" */ './views/User/index.vue'),
-    },
-    {
-      path: '/user/:username/asset',
-      name: 'Asset',
-      props: true,
-      component: () => import(/* webpackChunkName: "user-asset" */ './views/User/Asset.vue'),
-    },
-    {
-      path: '/user/:username/original',
-      name: 'Original',
-      props: true,
-      component: () => import(/* webpackChunkName: "user-original" */ './views/User/Original.vue'),
-    },
-    {
-      path: '/user/:username/reward',
-      name: 'Reward',
-      props: true,
-      component: () => import(/* webpackChunkName: "user-reward" */ './views/User/Reward.vue'),
-    },
-    {
-      path: '/publish',
-      name: 'Publish',
-      props: true,
-      component: () => import(/* webpackChunkName: "new-post" */ './views/Publish.vue'),
-    },
   ],
 });
