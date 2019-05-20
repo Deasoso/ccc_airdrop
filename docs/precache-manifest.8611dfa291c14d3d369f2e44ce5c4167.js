56c5d488a478b49f5ac0e0348b6bf77de22a3506
Deasoso <932152571@qq.com>
2019-05-21 00:37:14 +0800

    add docs.

---
+++ docs/precache-manifest.8611dfa291c14d3d369f2e44ce5c4167.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "742ef7badcbb97ad95b2",
+    "url": "css/app.ff9929b2.css"
+  },
+  {
+    "revision": "742ef7badcbb97ad95b2",
+    "url": "js/app.fe4c609b.js"
+  },
+  {
+    "revision": "056dd5da2aed75bf9bf0",
+    "url": "css/chunk-vendors.e7857349.css"
+  },
+  {
+    "revision": "056dd5da2aed75bf9bf0",
+    "url": "js/chunk-vendors.e7bf0092.js"
+  },
+  {
+    "revision": "8d4a4e6f7431d0d7fa92b1df20f38161",
+    "url": "fonts/fontello.8d4a4e6f.woff2"
+  },
+  {
+    "revision": "9354499c2824248511adf85fdf8e4c37",
+    "url": "img/fontello.9354499c.svg"
+  },
+  {
+    "revision": "e73a0647198cfe970de0f003be95cc51",
+    "url": "fonts/fontello.e73a0647.eot"
+  },
+  {
+    "revision": "a782baa8633b1359f9686ffad17e0d76",
+    "url": "fonts/fontello.a782baa8.woff"
+  },
+  {
+    "revision": "068ca2b316db98037bebdd1e4f1b9459",
+    "url": "fonts/fontello.068ca2b3.ttf"
+  },
+  {
+    "revision": "99ac3308dd8ee14f749f51538d0d5b9e",
+    "url": "fonts/ionicons.99ac3308.woff"
+  },
+  {
+    "revision": "d535a25a79fb1365ae814b61e88fae71",
+    "url": "fonts/ionicons.d535a25a.ttf"
+  },
+  {
+    "revision": "a2c4a261a239aa84463dc70e4bac9b9a",
+    "url": "img/ionicons.a2c4a261.svg"
+  },
+  {
+    "revision": "dd907496a79ef9c59d09e938cfac2238",
+    "url": "index.html"
+  },
+  {
+    "revision": "f34617f615e47cb56707b833f1de27cc",
+    "url": "img/Andoromeda logo@2x.png"
+  },
+  {
+    "revision": "55eaf707fdfd87a6e3d7c8321cdae145",
+    "url": "img/Andoromeda logo@3x.png"
+  },
+  {
+    "revision": "97495c8d353c4e8b84dd95fae198a617",
+    "url": "img/Andoromeda logo.png"
+  },
+  {
+    "revision": "dcc4f01805049e45e06aafeebe03ade2",
+    "url": "img/banner.png"
+  },
+  {
+    "revision": "fe41b0d761df83f0ef68c5764aa86c7a",
+    "url": "img/camera.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "7e58da8fe1e6726935f7822fc3f02a96",
+    "url": "img/20190314205225.jpg"
+  }
+];
